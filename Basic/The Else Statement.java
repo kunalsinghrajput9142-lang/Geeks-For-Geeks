c1438984541bd9c7aa1d7a9ee55248a0ea2cbe99
Kunal Kumar <kunalsinghrajput9142@gmail.com>
2026-04-14 21:53:26 +0530

    Add files via upload

---
+++ Basic/The Else Statement.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a>100?"Big":"Small");
+    }
+}
