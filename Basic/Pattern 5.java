2a2139a1aff55d65463cc10ee894c0f6b6833ec0
Kunal Kumar <kunalsinghrajput9142@gmail.com>
2026-04-22 22:53:15 +0530

    Add files via upload

---
+++ Basic/Pattern 5.java
@@ -0,0 +1,8 @@
+class Solution{
+    static void printTriangle(int n){
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++) System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
